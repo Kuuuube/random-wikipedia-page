716fda3c066624d437abea1d4c20d2c00fd7b43b
kuuuube <hexagonisalie@gmail.com>
2024-08-15 11:44:02 -0400

    Init with random page for browser lang

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-﻿document.addEventListener("DOMContentLoaded", requestOpenRandomPage);
+﻿const BROWSER_LANG = navigator.language || navigator.userLanguage;
 
-function requestOpenRandomPage() {
-    let lang = document.querySelector("#language").value;
+document.addEventListener("DOMContentLoaded", init_page);
+
+function init_page() {
+    let stripped_lang = BROWSER_LANG.replace(/-.*/, "");
+    open_page(BROWSER_LANG.replace(/-.*/, ""));
+    document.querySelector("#language").value = stripped_lang;
+}
+
+function random_page_button() {
+    open_page(document.querySelector("#language").value);
+}
+
+function open_page(lang) {
     let url = "https://" + lang + ".wikipedia.org/wiki/Special:Random";
-
     document.querySelector("#wikipedia_page_iframe").setAttribute("src", url);
 }
